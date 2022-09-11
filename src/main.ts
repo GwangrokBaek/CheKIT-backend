b01aea56e5f35ea4f209a2fd2096eb8ccc4ff0e7
GwangrokBaek <zester926@gmail.com>
2022-09-11 17:23:13 +0900

    Router 데코레이터 테스트용 example 추가

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import * as express from "express"
-import ApiRouter from "./routes/index"
+// import ApiRouter from "./routes/index"
+import TestRouter from "./routes/decoratorTest"
 
 export const app = express()
 
-app.use("/", new ApiRouter().router)
-
-// app.use(errorHandler)
+// app.use("/", new ApiRouter().router)
+new TestRouter()
 
 app.listen(3000, () => {
 	console.log("Server listening on port : 3000")
