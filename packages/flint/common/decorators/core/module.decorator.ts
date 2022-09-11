07628c1f18367c4c9de14c9b3f53df958824d9c6
GwangrokBaek <zester926@gmail.com>
2022-09-11 18:46:42 +0900

    서버 객체 생성 및 관리용 flint-initializer 구현 및 router metadata 추가 및 접근 코드 구현

--- packages/flint/common/decorators/core/module.decorator.ts
+++ packages/flint/common/decorators/core/module.decorator.ts
@@ -11,6 +11,13 @@ export function Module(metadata: IModuleMetadata): ClassDecorator {
 					target
 				)
 			}
+
+			if (property === "routers") {
+				for (const router of (metadata as any)[property]) {
+					const routerMetadata = Reflect.getMetadata("router", router)
+					Reflect.defineMetadata("router", routerMetadata, target)
+				}
+			}
 		}
 	}
 }
